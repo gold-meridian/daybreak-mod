b9f694d881b7248f2b4cb5dd58e64cfef46b68f1
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-26 03:56:19 -0500

    feat: add render target preserver to daybreak

---
+++ src/Daybreak/Common/Rendering/RenderTargetPreserver.cs
@@ -0,0 +1,56 @@
+﻿using Daybreak.Common.Features.Hooks;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+
+namespace Daybreak.Common.Rendering;
+
+/// <summary>
+///     Handles preserving the contents of <see cref="RenderTarget2D"/>s.
+/// </summary>
+public static class RenderTargetPreserver
+{
+    /// <summary>
+    ///     Forcefully sets the usage of a given set of render target bindings to
+    ///     preserve contents.
+    /// </summary>
+    /// <param name="bindings"></param>
+    public static void PreserveBindings(RenderTargetBinding[] bindings)
+    {
+        foreach (var binding in bindings)
+        {
+            if (binding.RenderTarget is not RenderTarget2D rt)
+            {
+                continue;
+            }
+
+            rt.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+        }
+    }
+
+    /// <summary>
+    ///     A utility method that gets the current bindings from the graphics
+    ///     device and ensures they're preserved.
+    /// </summary>
+    /// <returns></returns>
+    public static RenderTargetBinding[] GetAndPreserveCurrentBindings()
+    {
+        var bindings = Main.instance.GraphicsDevice.GetRenderTargets();
+        {
+            PreserveBindings(bindings);
+        }
+
+        return bindings;
+    }
+
+    [OnLoad]
+    private static void Load()
+    {
+        Main.RunOnMainThread(
+            () =>
+            {
+                Main.graphics.GraphicsDevice.PresentationParameters.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+                Main.graphics.ApplyChanges();
+            }
+        );
+    }
+}
