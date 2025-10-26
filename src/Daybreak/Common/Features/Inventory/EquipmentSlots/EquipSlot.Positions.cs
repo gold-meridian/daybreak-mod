00b96d0df2a75f436a189501c01883e747971639
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-26 04:30:03 -0500

    feat: quick cleanup

--- src/Daybreak/Common/Features/Inventory/EquipmentSlots/EquipSlot.Positions.cs
+++ src/Daybreak/Common/Features/Inventory/EquipmentSlots/EquipSlot.Positions.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace Daybreak.Common.Features.Inventory;
 
 partial class EquipSlot
--- src/Daybreak/Common/Features/Inventory/EquipmentSlots/EquipSlotLoader.cs
+++ src/Daybreak/Common/Features/Inventory/EquipmentSlots/EquipSlotLoader.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using MonoMod.Cil;
 using Terraria;
