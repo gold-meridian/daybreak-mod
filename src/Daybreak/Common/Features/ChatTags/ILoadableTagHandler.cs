f9d23367fa68c57fe16189383382941a36430029
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-28 01:51:36 -0500

    feat: make public and add summaries

--- src/Daybreak/Common/Features/ChatTags/ILoadableTagHandler.cs
+++ src/Daybreak/Common/Features/ChatTags/ILoadableTagHandler.cs
@@ -4,10 +4,16 @@ using Terraria.UI.Chat;
 
 namespace Daybreak.Common.Features.ChatTags;
 
+/// <summary>
+///     Automatically registers itself as a chat tag.
+/// </summary>
 [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature, ImplicitUseTargetFlags.WithInheritors)]
-internal interface ILoadableTagHandler<TSelf> : ITagHandler, ILoadable
+public interface ILoadableTagHandler<TSelf> : ITagHandler, ILoadable
     where TSelf : ILoadableTagHandler<TSelf>, new()
 {
+    /// <summary>
+    ///     The tag names.
+    /// </summary>
     string[] TagNames { get; }
 
     void ILoadable.Load(Mod mod)
