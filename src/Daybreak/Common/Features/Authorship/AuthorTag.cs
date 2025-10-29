ed5b73ac17e99eb54e94365cf5a52239219f8b1b
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-29 01:49:53 -0500

    feat: make author names populate localization files

--- src/Daybreak/Common/Features/Authorship/AuthorTag.cs
+++ src/Daybreak/Common/Features/Authorship/AuthorTag.cs
@@ -56,6 +56,8 @@ public abstract class AuthorTag : ModTexturedType, ILocalizedModType
     {
         base.SetupContent();
 
+        _ = DisplayName;
+
         SetStaticDefaults();
     }
 #endregion
