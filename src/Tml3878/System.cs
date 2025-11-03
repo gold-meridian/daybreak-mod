3ee885380fb1bfa72837290b46e88d3c3c3fe806
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-11-03 01:27:48 -0600

    feat: reproduce tml 3878

---
+++ src/Tml3878/System.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using MonoMod.Cil;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Tml3878;
+
+internal sealed class System : ModSystem
+{
+    private static readonly int random_value = new Random().Next(1000, 10000);
+
+    public override void Load()
+    {
+        base.Load();
+
+        Task.Run(
+            async () =>
+            {
+                var item = new Item(ItemID.IronPickaxe);
+                Mod.Logger.Info($"item damage is 5: {item.damage == 5}");
+
+                Mod.Logger.Info("Installing hook...");
+                IL_Item.SetDefaults1 += il =>
+                {
+                    // change damage of iron pickaxe as really simple test case
+                    var c = new ILCursor(il);
+                    c.GotoNext(MoveType.After, x => x.MatchLdcI4(5));
+                    c.EmitPop();
+                    c.EmitLdcI4(random_value);
+                };
+                Mod.Logger.Info("Installed hook!");
+
+                while (true)
+                {
+                    item.SetDefaults(ItemID.IronPickaxe);
+                    Mod.Logger.Info($"item damage is random value: {item.damage == random_value} ({item.damage})");
+                    await Task.Delay(250);
+                }
+                
+                // ReSharper disable once FunctionNeverReturns
+            }
+        );
+    }
+}
