b25f36ae10629f013b60acbea9caa5a7d38419e0
mayli4 <sydsleeper@gmail.com>
2025-11-05 01:21:45 -0500

    wip: init pipeline stubs

---
+++ src/Daybreak/Common/Rendering/Pipelines/Pipelines.cs
@@ -0,0 +1,17 @@
+﻿namespace Daybreak.Common.Rendering.Pipelines;
+
+/// <summary>
+/// 
+/// </summary>
+public struct Pipeline
+{
+    
+}
+
+/// <summary>
+/// Stores commands to be submitted to a <see cref="Pipeline"/>
+/// </summary>
+public struct Commands
+{
+    
+}
