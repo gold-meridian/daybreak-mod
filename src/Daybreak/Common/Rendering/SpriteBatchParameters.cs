64568c16ae473d0ffdbf22a6258ac96fda0e599e
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-11-06 02:36:41 -0600

    feat: effect chaining

---
+++ src/Daybreak/Common/Rendering/SpriteBatchParameters.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Daybreak.Common.Rendering;
+
+/// <summary>
+///     Wraps the parameters of a <see cref="SpriteBatch"/> initialization,
+///     permitting <see langword="null"/> values which may be replaced with
+///     defaults when being converted to a snapshot.
+/// </summary>
+public struct SpriteBatchParameters
+{
+    /// <summary>
+    ///     The sort mode.
+    /// </summary>
+    public SpriteSortMode? SortMode { get; set; }
+
+    /// <summary>
+    ///     The blend state.
+    /// </summary>
+    public BlendState? BlendState { get; set; }
+
+    /// <summary>
+    ///     The sampler state.
+    /// </summary>
+    public SamplerState? SamplerState { get; set; }
+
+    /// <summary>
+    ///     The depth stencil state.
+    /// </summary>
+    public DepthStencilState? DepthStencilState { get; set; }
+
+    /// <summary>
+    ///     The rasterizer state.
+    /// </summary>
+    public RasterizerState? RasterizerState { get; set; }
+
+    /// <summary>
+    ///     The custom effect, if applicable.
+    /// </summary>
+    public Effect? CustomEffect { get; set; }
+
+    /// <summary>
+    ///     The transformation matrix.
+    /// </summary>
+    public Matrix? TransformMatrix { get; set; }
+
+    /// <summary>
+    ///     Initializes a new set of parameters.
+    /// </summary>
+    public SpriteBatchParameters(
+        SpriteSortMode? sortMode = null,
+        BlendState? blendState = null,
+        SamplerState? samplerState = null,
+        DepthStencilState? depthStencilState = null,
+        RasterizerState? rasterizerState = null,
+        Effect? customEffect = null,
+        Matrix? transformMatrix = null
+    )
+    {
+        SortMode = sortMode;
+        BlendState = blendState;
+        SamplerState = samplerState;
+        DepthStencilState = depthStencilState;
+        RasterizerState = rasterizerState;
+        CustomEffect = customEffect;
+        TransformMatrix = transformMatrix;
+    }
+    
+    /// <summary>
+    ///     Creates a new <see cref="SpriteBatchSnapshot"/>, with
+    ///     <see langword="null"/> values being replaced with the values
+    ///     provided by <paramref name="defaultValues"/>.
+    /// </summary>
+    /// <param name="defaultValues">
+    ///     Default values to populate <see langword="null"/>s with.
+    /// </param>
+    /// <returns>The new snapshot.</returns>
+    public SpriteBatchSnapshot ToSnapshot(SpriteBatchSnapshot defaultValues)
+    {
+        return new SpriteBatchSnapshot(
+            SortMode ?? defaultValues.SortMode,
+            BlendState ?? defaultValues.BlendState,
+            SamplerState ?? defaultValues.SamplerState,
+            DepthStencilState ?? defaultValues.DepthStencilState,
+            RasterizerState ?? defaultValues.RasterizerState,
+            CustomEffect ?? defaultValues.CustomEffect,
+            TransformMatrix ?? defaultValues.TransformMatrix
+        );
+    }
+}
--- src/Daybreak/Common/Rendering/SpriteBatchSnapshot.cs
+++ src/Daybreak/Common/Rendering/SpriteBatchSnapshot.cs
@@ -9,9 +9,6 @@ namespace Daybreak.Common.Rendering;
 ///     <br />
 ///     These values may be manipulated freely.
 /// </summary>
-/// <param name="spriteBatch">
-///     The <see cref="SpriteBatch" /> to take a snapshot of.
-/// </param>
 /// <remarks>
 ///     This API exists for making preservation of a <see cref="SpriteBatch" />'s
 ///     state trivial.
@@ -23,42 +20,99 @@ namespace Daybreak.Common.Rendering;
 ///     <see cref="SpriteBatchSnapshotExtensions" />.
 /// </remarks>
 [PublicAPI]
-public struct SpriteBatchSnapshot(SpriteBatch spriteBatch)
+public struct SpriteBatchSnapshot
 {
     /// <summary>
     ///     The sort mode.
     /// </summary>
-    public SpriteSortMode SortMode { get; set; } = spriteBatch.sortMode;
+    public SpriteSortMode SortMode { get; set; }
 
     /// <summary>
     ///     The blend state.
     /// </summary>
-    public BlendState BlendState { get; set; } = spriteBatch.blendState;
+    public BlendState BlendState { get; set; }
 
     /// <summary>
     ///     The sampler state.
     /// </summary>
-    public SamplerState SamplerState { get; set; } = spriteBatch.samplerState;
+    public SamplerState SamplerState { get; set; }
 
     /// <summary>
     ///     The depth stencil state.
     /// </summary>
-    public DepthStencilState DepthStencilState { get; set; } = spriteBatch.depthStencilState;
+    public DepthStencilState DepthStencilState { get; set; }
 
     /// <summary>
     ///     The rasterizer state.
     /// </summary>
-    public RasterizerState RasterizerState { get; set; } = spriteBatch.rasterizerState;
+    public RasterizerState RasterizerState { get; set; }
 
     /// <summary>
     ///     The custom effect, if applicable.
     /// </summary>
-    public Effect? CustomEffect { get; set; } = spriteBatch.customEffect;
+    public Effect? CustomEffect { get; set; }
 
     /// <summary>
     ///     The transformation matrix.
     /// </summary>
-    public Matrix TransformMatrix { get; set; } = spriteBatch.transformMatrix;
+    public Matrix TransformMatrix { get; set; }
+
+    /// <summary>
+    ///     Creates a new <see cref="SpriteBatch"/> snapshot from raw
+    ///     parameters.
+    /// </summary>
+    public SpriteBatchSnapshot(
+        SpriteSortMode sortMode,
+        BlendState blendState,
+        SamplerState samplerState,
+        DepthStencilState depthStencilState,
+        RasterizerState rasterizerState,
+        Effect? customEffect,
+        Matrix transformMatrix
+    )
+    {
+        SortMode = sortMode;
+        BlendState = blendState;
+        SamplerState = samplerState;
+        DepthStencilState = depthStencilState;
+        RasterizerState = rasterizerState;
+        CustomEffect = customEffect;
+        TransformMatrix = transformMatrix;
+    }
+
+    /// <summary>
+    ///     Creates a new <see cref="SpriteBatch"/> snapshot from the current
+    ///     settings of the given <see cref="SpriteBatch"/>.
+    /// </summary>
+    /// <param name="spriteBatch">
+    ///     The <see cref="SpriteBatch" /> to take a snapshot of.
+    /// </param>
+    public SpriteBatchSnapshot(SpriteBatch spriteBatch)
+    {
+        SortMode = spriteBatch.sortMode;
+        BlendState = spriteBatch.blendState;
+        SamplerState = spriteBatch.samplerState;
+        DepthStencilState = spriteBatch.depthStencilState;
+        RasterizerState = spriteBatch.rasterizerState;
+        CustomEffect = spriteBatch.customEffect;
+        TransformMatrix = spriteBatch.transformMatrix;
+    }
+
+    /// <summary>
+    ///     Initializes a set of parameters from this snapshot.
+    /// </summary>
+    public SpriteBatchParameters ToParameters()
+    {
+        return new SpriteBatchParameters(
+            SortMode,
+            BlendState,
+            SamplerState,
+            DepthStencilState,
+            RasterizerState,
+            CustomEffect,
+            TransformMatrix
+        );
+    }
 }
 
 /// <summary>
