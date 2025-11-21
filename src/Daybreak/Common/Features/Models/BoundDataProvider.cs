f3020dda3755c3fb239c17d31291d6c60a2a9b86
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-11-21 02:01:26 -0600

    feat: name providers

--- src/Daybreak/Common/Features/Models/BoundDataProvider.cs
+++ src/Daybreak/Common/Features/Models/BoundDataProvider.cs
@@ -10,30 +10,65 @@ namespace Daybreak.Common.Features.Models;
 ///     Abstraction designed to handle a collection of <see cref="Bound{T}"/>
 ///     objects.
 /// </summary>
-public abstract class BoundDataProvider : InstanceData
+public abstract class BoundDataProvider : ModType
 {
-    /// <inheritdoc />
-    protected override void LoadSingleton(Mod mod)
-    {
-        var properties = GetType()
-                               .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                               .Where(x => x.PropertyType.IsAssignableTo(typeof(IBinding)))
-                               .Select(x => x.GetValue(this))
-                               .Cast<IBound>()
-                               .ToArray();
-
-        mod.AddContent(CreateContent(properties));
-    }
-
-    /// <inheritdoc />
-    protected override void UnloadSingleton()
-    {
-        // TODO
-    }
-
     /// <summary>
-    ///     Initializes a loadable type to be added to the mod.
+    ///     The name of this data provider, which is used for things such as
+    ///     data serialization.
     /// </summary>
-    /// <param name="properties">The built collection of properties.</param>
-    protected abstract ILoadable CreateContent(IBound[] properties);
+    public override string Name { get; }
+
+    /// <inheritdoc />
+    protected BoundDataProvider()
+    {
+        Name = NameProvider.ForType(GetType());
+    }
+
+    /// <inheritdoc />
+    public sealed override void Load()
+    {
+        base.Load();
+
+        var properties = GetType()
+                        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .Where(x => x.PropertyType.IsAssignableTo(typeof(IBinding)))
+                        .Select(x => x.GetValue(this))
+                        .Cast<IBound>()
+                        .ToArray();
+    }
+
+    /// <inheritdoc />
+    public sealed override void Unload()
+    {
+        base.Unload();
+    }
+
+#region Unused hooks
+    /// <inheritdoc />
+    protected sealed override void Register() { }
+
+    /// <inheritdoc />
+    public sealed override void SetupContent()
+    {
+        base.SetupContent();
+    }
+
+    /// <inheritdoc />
+    public sealed override void SetStaticDefaults()
+    {
+        base.SetStaticDefaults();
+    }
+
+    /// <inheritdoc />
+    protected sealed override void InitTemplateInstance()
+    {
+        base.InitTemplateInstance();
+    }
+
+    /// <inheritdoc />
+    protected sealed override void ValidateType()
+    {
+        base.ValidateType();
+    }
+#endregion
 }
---
+++ src/Daybreak/Common/Features/Models/NameProvision.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Terraria.ModLoader;
+
+namespace Daybreak.Common.Features.Models;
+
+/// <summary>
+///     Utilities for getting names from name providers.
+/// </summary>
+public static class NameProvider
+{
+    /// <summary>
+    ///     Returns the name provided by an
+    ///     <see cref="AbstractNameProviderAttribute"/> if the type is annotated
+    ///     with one, otherwise <see cref="Type.Name"/>.
+    /// </summary>
+    public static string ForType(Type type)
+    {
+        var nameProvider = type.GetCustomAttribute<AbstractNameProviderAttribute>();
+        return nameProvider?.GetName(type) ?? GetDefaultName(type);
+    }
+
+    /// <summary>
+    ///     The default name for this type, which is <see cref="Type.Name"/>.
+    /// </summary>
+    public static string GetDefaultName(Type type)
+    {
+        return type.Name;
+    }
+}
+
+/// <summary>
+///     Used to provide a name to a <see cref="ModType"/>.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+public abstract class AbstractNameProviderAttribute : Attribute
+{
+    /// <summary>
+    ///     Produces a name for the given type.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>The name.</returns>
+    public abstract string GetName(Type type);
+}
+
+/// <summary>
+///     Provides the given <paramref name="name"/> as the name.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+public sealed class NameProviderAttribute(string name) : AbstractNameProviderAttribute
+{
+    /// <inheritdoc />
+    public override string GetName(Type type)
+    {
+        return name;
+    }
+}
+
+/// <summary>
+///     Provides a name including the parent types walking up a nested type tree
+///     to generate a reliable, unique name for commonly-named nested types.
+/// </summary>
+/// <param name="name">
+///     The name.  If left <see langword="null"/>, the type's name will be used.
+/// </param>
+public sealed class NestedTypeNameProviderAttribute(string? name = null) : AbstractNameProviderAttribute
+{
+    /// <inheritdoc />
+    public override string GetName(Type type)
+    {
+        var typeName = name ?? NameProvider.GetDefaultName(type);
+        var nestedNames = GetNestedNames(type).Append(typeName);
+        var nestedName = string.Join('+', nestedNames);
+        return nestedName;
+    }
+
+    private static IEnumerable<string> GetNestedNames(Type type)
+    {
+        var names = new List<string>();
+
+        while (type.DeclaringType is not null)
+        {
+            names.Add(type.DeclaringType.Name);
+            type = type.DeclaringType;
+        }
+
+        return names.AsEnumerable().Reverse();
+    }
+}
