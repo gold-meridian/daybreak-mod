796af6e72c1f844e14e8ae4994f4d796f84f64e4
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-11-23 23:05:08 -0600

    feat: also move invalidhookstacking

--- src/Daybreak.CodeAnalysis/Analyzers/Hooks/InvalidHookStacking.cs
+++ src/Daybreak.CodeAnalysis/Analyzers/Hooks/InvalidHookStacking.cs
@@ -1,3 +1,62 @@
-﻿namespace Daybreak.CodeAnalysis;
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
 
-public static class InvalidHookStacking { }
+namespace Daybreak.CodeAnalysis;
+
+public static class InvalidHookStacking
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class Analyzer() : AbstractDiagnosticAnalyzer(Diagnostics.InvalidHookStacking)
+    {
+        protected override void InitializeWorker(AnalysisContext ctx)
+        {
+            ctx.RegisterCompilationStartAction(
+                static startCtx =>
+                {
+                    if (!startCtx.Compilation.TryGetHookAttributes(out var attrs))
+                    {
+                        return;
+                    }
+
+                    startCtx.RegisterSymbolAction(
+                        symbolCtx =>
+                        {
+                            if (symbolCtx.Symbol is not IMethodSymbol symbol)
+                            {
+                                return;
+                            }
+
+                            var attributes = symbol.GetAttributes();
+                            if (attributes.Length == 0)
+                            {
+                                return;
+                            }
+
+                            var hookDefinitions = attributes.GetHooks(attrs).ToArray();
+                            if (hookDefinitions.Length < 2)
+                            {
+                                return;
+                            }
+
+                            var legal = hookDefinitions.Distinct().All(x => x.ValidateMultiple(hookDefinitions));
+                            if (legal)
+                            {
+                                return;
+                            }
+
+                            symbolCtx.ReportDiagnostic(
+                                Diagnostic.Create(
+                                    Diagnostics.InvalidHookStacking,
+                                    symbol.Locations[0],
+                                    symbol.ToDisplayString(SymbolDisplayFormat.CSharpErrorMessageFormat)
+                                )
+                            );
+                        },
+                        SymbolKind.Method
+                    );
+                }
+            );
+        }
+    }
+}
