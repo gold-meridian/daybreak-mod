8b99976cffb6f6bdc2775ffc5e778d449734b05d
ZenTheMod <88523932+ZenTheMod@users.noreply.github.com>
2025-12-12 19:29:58 -0800

    port over ZenSkies input system

---
+++ src/Daybreak/Common/UI/InputField.cs
@@ -0,0 +1,150 @@
+﻿using Daybreak.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PrettyRarities.Core;
+using ReLogic.Graphics;
+using System;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
+using Terraria.UI.Chat;
+
+namespace Daybreak.Common.UI;
+
+public class InputField : UIPanel
+{
+    protected int mousePosition;
+
+    protected bool writing;
+
+    public string OldText = string.Empty;
+
+    public string Text;
+
+    public string Hint;
+
+    public int MaxChars;
+
+    public char[] BlacklistedChars = [];
+
+    public char[] WhitelistedChars = [];
+
+    public float TextAlignX;
+
+    public event Action<InputField>? OnEnter;
+
+    public event Action<InputField>? OnEscape;
+
+    public InputField(string hint, int maxChars)
+    {
+        Text = string.Empty;
+        Hint = hint;
+        MaxChars = maxChars;
+
+        _backgroundTexture = AssetReferences.Assets.Images.UI.EmptyPanel.Asset;
+        BorderColor = Color.Transparent;
+
+        Width.Set(0f, 1f);
+        Height.Set(26f, 0f);
+
+        SetPadding(6);
+    }
+
+    public override void LeftMouseDown(UIMouseEvent evt)
+    {
+        base.LeftMouseDown(evt);
+
+        if (evt.Target != this)
+        {
+            writing = false;
+            return;
+        }
+
+        writing = true;
+        OldText = Text;
+
+        InputHelpers.CursorPositon = 0;
+
+        if (Text.Length <= 0)
+            return;
+
+        InputHelpers.CursorPositon = mousePosition;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+        if (Text.Length > MaxChars)
+        {
+            Text = Text[..MaxChars];
+        }
+
+        bool clickedOff =
+            !Main.hasFocus ||
+            (Main.mouseLeft &&
+            !IsMouseHovering);
+
+        if (!clickedOff || !writing)
+        {
+            return;
+        }
+
+        // Should clicking off the panel count as confirming?
+        // OnEnter?.Invoke(this);
+        writing = false;
+    }
+
+    private void HandleInput()
+    {
+        InputHelpers.WritingText = true;
+
+        switch (InputHelpers.GetInput(Text, out string newText, false, BlacklistedChars, WhitelistedChars))
+        {
+            case InputCancellationType.Confirmed:
+                Text = newText;
+                OnEnter?.Invoke(this);
+                writing = false;
+                break;
+
+            case InputCancellationType.Escaped:
+                Text = OldText;
+                OnEscape?.Invoke(this);
+                writing = false;
+                break;
+
+            default:
+                Text = newText;
+                break;
+        }
+    }
+
+    protected override void DrawSelf(SpriteBatch spriteBatch)
+    {
+        base.DrawSelf(spriteBatch);
+
+        DynamicSpriteFont font = FontAssets.MouseText.Value;
+
+        Rectangle dims = this.InnerDimensions;
+
+        Vector2 position = new(dims.X + (dims.Width * TextAlignX), dims.Y + (dims.Height * .5f) + 4);
+
+        Vector2 textSize = font.MeasureString(Text == string.Empty ? Hint : Text);
+        Vector2 origin = new(textSize.X * TextAlignX, textSize.Y * .5f);
+
+        bool drawBlinker = writing && Main.GlobalTimeWrappedHourly % .666f > .333f;
+
+        if (Text == string.Empty)
+        {
+            ChatManager.DrawColorCodedStringWithShadow(spriteBatch, font, Hint, position, Color.Gray, 0f, origin, Vector2.One);
+        }
+
+        spriteBatch.DrawInputStringWithShadow(UserInterface.ActiveInstance.MousePosition, font, Text, position, Color.White, origin, Vector2.One, out mousePosition, drawBlinker, InputHelpers.CursorPositon);
+
+        if (writing)
+        {
+            HandleInput();
+        }
+    }
+}
---
+++ src/Daybreak/Common/UI/InputHelpers.cs
@@ -0,0 +1,446 @@
+﻿using Daybreak.Common.Features.Hooks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using ReLogic.Graphics;
+using ReLogic.Localization.IME;
+using ReLogic.OS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.GameInput;
+using Terraria.Initializers;
+using Terraria.UI.Chat;
+using static ReLogic.Graphics.DynamicSpriteFont;
+
+namespace Daybreak.Common.UI;
+
+internal enum InputCancellationType : byte
+{
+    None,
+    Escaped,
+    Confirmed
+}
+
+/// <summary>
+/// Simpler input system based on Vanilla's <see cref="Main.GetInputText"/>;
+/// with logic for input cancellation via the <see cref="InputCancellationType"/> enum.
+/// </summary>
+internal static class InputHelpers
+{
+    private const int max_stroke_length = 20;
+
+    private const int key_timer_delay = 45;
+
+    private static readonly char[] invalidChars =
+        [.. Enumerable.Range('\x0', '\x1F' + 1).Select(i => (char)i), // Invisible characters from Null to Unit Separator, stopping before Space.
+        '\x7F' // Delete.
+        ];
+
+    private static int backspaceTimer = key_timer_delay;
+
+    private static int leftArrowTimer = key_timer_delay;
+
+    private static int rightArrowTimer = key_timer_delay;
+
+    private static string keyStroke = string.Empty;
+
+    // Stores the state of WritingText for use outside of drawing scopes.
+    private static bool wasWritingText;
+
+    public static bool WritingText
+    {
+        get => PlayerInput.WritingText;
+        set
+        {
+            wasWritingText |= value;
+            PlayerInput.WritingText = value;
+        }
+    }
+
+    /// <summary>
+    /// The index of the "cursor" where written text is inserted.
+    /// </summary>
+    public static int CursorPositon { get; set; }
+
+    [OnLoad]
+    public static void Load()
+    {
+        On_Main.DoUpdate_HandleInput += DoUpdate_HandleInput_UpdateWasWritingText;
+        On_UILinksInitializer.FancyExit += FancyExit_IgnoreExitIfWriting;
+        Platform.Get<IImeService>().AddKeyListener(OnKeyStroke);
+    }
+
+    [OnUnload]
+    public static void Unload()
+    {
+        Platform.Get<IImeService>().RemoveKeyListener(OnKeyStroke);
+    }
+
+    private static void DoUpdate_HandleInput_UpdateWasWritingText(On_Main.orig_DoUpdate_HandleInput orig, Main self)
+    {
+        wasWritingText = WritingText;
+        orig(self);
+    }
+
+    private static void OnKeyStroke(char key)
+    {
+        if (WritingText &&
+            keyStroke.Length <= max_stroke_length)
+            keyStroke += key;
+    }
+
+    private static void FancyExit_IgnoreExitIfWriting(On_UILinksInitializer.orig_FancyExit orig)
+    {
+        if (!wasWritingText)
+        {
+            orig();
+        }
+    }
+
+    public static InputCancellationType GetInput(
+        string input,
+        out string output,
+        bool allowLineBreaks = false,
+        IEnumerable<char>? blacklistedChars = null,
+        IEnumerable<char>? whitelistedChars = null)
+    {
+        output = input;
+
+        // Perhaps use our own KeyboardStates?
+        Main.oldInputText = Main.inputText;
+        Main.inputText = Keyboard.GetState();
+
+        WritingText &= Main.hasFocus;
+
+        if (!WritingText)
+            return InputCancellationType.None;
+
+        Main.instance.HandleIME();
+
+        blacklistedChars ??= [];
+        blacklistedChars = blacklistedChars.Concat(invalidChars);
+
+        // Arrow key movement
+        {
+            // Left
+            if (Keys.Left.Held)
+            {
+                leftArrowTimer--;
+            }
+            else
+            {
+                leftArrowTimer = key_timer_delay;
+            }
+
+            if (Keys.Left.JustPressed ||
+                (Keys.Left.Held &&
+                leftArrowTimer <= 0))
+            {
+                CursorPositon--;
+            }
+
+            // Right
+            if (Keys.Right.Held)
+            {
+                rightArrowTimer--;
+            }
+            else
+            {
+                rightArrowTimer = key_timer_delay;
+            }
+            if (Keys.Right.JustPressed ||
+                (Keys.Right.Held &&
+                rightArrowTimer <= 0))
+            {
+                CursorPositon++;
+            }
+        }
+
+        CursorPositon = Math.Clamp(CursorPositon, 0, output.Length);
+
+        // Special actions
+        {
+            bool controlPressed =
+                (Keys.LeftControl.Pressed ||
+                Keys.RightControl.Pressed) &&
+                !Keys.LeftAlt.Pressed &&
+                !Keys.RightAlt.Pressed;
+
+            bool shiftPressed =
+                Keys.LeftShift.Pressed ||
+                Keys.RightShift.Pressed;
+
+            // Clear
+            if (controlPressed && Keys.Z.JustPressed)
+            {
+                output = string.Empty;
+                CursorPositon = 0;
+            }
+            // Cut
+            else if (
+                (controlPressed && Keys.X.JustPressed) ||
+                (shiftPressed && Keys.Delete.JustPressed))
+            {
+                Platform.Get<IClipboard>().Value = output;
+                output = string.Empty;
+                CursorPositon = 0;
+            }
+            // Copy
+            else if (Keys.C.JustPressed)
+            {
+                Platform.Get<IClipboard>().Value = output;
+            }
+            // Paste
+            else if (
+                (controlPressed && Keys.V.JustPressed) ||
+                (shiftPressed && Keys.Insert.JustPressed))
+            {
+                string paste = RemoveInvalidCharacters(GetPaste(output, allowLineBreaks));
+
+                output = output.Insert(CursorPositon, paste);
+
+                CursorPositon += paste.Length;
+            }
+        }
+
+        // Input
+        {
+            if (keyStroke.Length >= 1)
+            {
+                string stroke = RemoveInvalidCharacters(keyStroke);
+
+                keyStroke = string.Empty;
+
+                output = output.Insert(CursorPositon, stroke);
+
+                CursorPositon += stroke.Length;
+            }
+        }
+
+        // Backspace
+        {
+            if (Keys.Back.Held)
+            {
+                backspaceTimer--;
+            }
+            else
+            {
+                backspaceTimer = key_timer_delay;
+            }
+            if ((Keys.Back.JustPressed ||
+                (Keys.Back.Held &&
+                backspaceTimer <= 0)) &&
+                output.Length >= 1 &&
+                CursorPositon >= 1)
+            {
+                output = string.Concat(output.AsSpan(0, CursorPositon - 1),
+                    output.AsSpan(CursorPositon, output.Length - CursorPositon));
+
+                CursorPositon--;
+            }
+        }
+
+        // Escapes
+        {
+            // Unsure of why vanilla checks if you're on Windows before allowing escape inputs.
+            // if (!Platform.IsWindows && Main.inputText.IsKeyDown(Keys.Escape) && !Main.oldInputText.IsKeyDown(Keys.Escape))
+            if (Keys.Escape.JustPressed)
+            {
+                // Definitly sketchy, but is designed to prevent UI from vanishing whilst typing.
+                PlayerInput.WritingText = false;
+                return InputCancellationType.Escaped;
+            }
+
+            if (Keys.Enter.JustPressed)
+            {
+                WritingText = false;
+                return InputCancellationType.Confirmed;
+            }
+        }
+
+        return InputCancellationType.None;
+
+        string RemoveInvalidCharacters(string input)
+        {
+            foreach (char c in blacklistedChars)
+            {
+                input = input.Replace(c.ToString(), string.Empty);
+            }
+
+            if (whitelistedChars is not null &&
+                whitelistedChars.Any())
+            {
+                input = new(input.Where(c => whitelistedChars.Contains(c)).ToArray());
+            }
+
+            return input;
+        }
+    }
+
+    private static string GetPaste(string input, bool allowLineBreaks)
+    {
+        return input.Insert(
+                            CursorPositon,
+                            allowLineBreaks ?
+                                Platform.Get<IClipboard>().MultiLineValue :
+                                Platform.Get<IClipboard>().Value);
+    }
+
+    /// <summary>
+    /// Ultimately slower version of typical string drawing methods, that allows for a cursor to be drawn in-between characters.<br/>
+    /// Made for use with <c>Input.GetInput</c>, and <see cref="CursorPositon"/> as <paramref name="blinkerIndex"/>.
+    /// </summary>
+    /// <param name="mousePosition">Position that should be checked to find <paramref name="hoveredChar"/>.</param>
+    /// <param name="drawBlinker">Weither or not the cursor/"blinker" should be drawn, best used with a timer.</param>
+    /// <param name="blinkerIndex">The index at which the blinker should be drawn, usually <see cref="CursorPositon"/>.</param>
+    public static void DrawInputString(
+        this SpriteBatch spriteBatch,
+        Vector2 mousePosition,
+        DynamicSpriteFont font,
+        string text,
+        Vector2 position,
+        Color color,
+        Vector2 origin,
+        Vector2 scale,
+        out int hoveredChar,
+        bool drawBlinker = false,
+        int blinkerIndex = -1) =>
+        spriteBatch.DrawInputStringWithShadow(mousePosition, font, text, position, color, Color.Black, origin, scale, out hoveredChar, drawBlinker, blinkerIndex, -1f);
+
+    /// <inheritdoc cref="DrawInputString"/>
+    public static void DrawInputStringWithShadow(
+        this SpriteBatch spriteBatch,
+        Vector2 mousePosition,
+        DynamicSpriteFont font,
+        string text,
+        Vector2 position,
+        Color color,
+        Vector2 origin,
+        Vector2 scale,
+        out int hoveredChar,
+        bool drawBlinker = false,
+        int blinkerIndex = -1,
+        float spread = 2f) =>
+        spriteBatch.DrawInputStringWithShadow(mousePosition, font, text, position, color, Color.Black, origin, scale, out hoveredChar, drawBlinker, blinkerIndex, spread);
+
+    /// <inheritdoc cref="DrawInputString"/>
+    public static void DrawInputStringWithShadow(
+        this SpriteBatch spriteBatch,
+        Vector2 mousePosition,
+        DynamicSpriteFont font,
+        string text,
+        Vector2 position,
+        Color color,
+        Color shadowColor,
+        Vector2 origin,
+        Vector2 scale,
+        out int hoveredChar,
+        bool drawBlinker = false,
+        int blinkerIndex = -1,
+        float spread = 2f)
+    {
+        bool first = true;
+        float lastKerning = 0f;
+
+        hoveredChar = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            char c = text[i];
+
+            spriteBatch.DrawStringWithShadow(font, c.ToString(), position, color, shadowColor, 0f, origin, scale, spread);
+
+            if (drawBlinker &&
+                i == blinkerIndex)
+            {
+                Vector2 blinkerPosition = new(position.X - (2f * scale.X), position.Y);
+
+                spriteBatch.DrawStringWithShadow(font, "|", blinkerPosition, color, shadowColor, 0f, origin, scale, spread);
+            }
+
+            Vector2 charSize = font.MeasureChar(c, first, lastKerning, out lastKerning);
+
+            if (mousePosition.X >= position.X && mousePosition.X <= position.X + charSize.X)
+                hoveredChar = mousePosition.X >= position.X + (charSize.X * .5f) ? i + 1 : i;
+
+            position.X += font.MeasureChar(c, first, lastKerning, out lastKerning).X;
+            first = false;
+        }
+
+        if (mousePosition.X >= position.X)
+            hoveredChar = text.Length;
+
+        if (drawBlinker &&
+            blinkerIndex >= text.Length)
+        {
+            Vector2 blinkerPosition = new(position.X - (2f * scale.X), position.Y);
+
+            spriteBatch.DrawStringWithShadow(font, "|", blinkerPosition, color, shadowColor, 0f, origin, scale, spread);
+        }
+    }
+
+    // Notably we don't use ChatManager to avoid allowing the player to type chat tags.
+    private static void DrawStringWithShadow(this SpriteBatch spriteBatch,
+        DynamicSpriteFont font,
+        string text,
+        Vector2 position,
+        Color color,
+        Color shadowColor,
+        float rotation,
+        Vector2 origin,
+        Vector2 scale,
+        float spread = 2f)
+    {
+        if (spread > 0f)
+        {
+            for (int i = 0; i < ChatManager.ShadowDirections.Length; i++)
+                spriteBatch.DrawString(font, text, position + ChatManager.ShadowDirections[i] * spread, shadowColor, rotation, origin, scale, SpriteEffects.None, 0f);
+        }
+
+        spriteBatch.DrawString(font, text, position, color, rotation, origin, scale, SpriteEffects.None, 0f);
+    }
+
+    private static Vector2 MeasureChar(this DynamicSpriteFont font, char c, bool firstChar, float lastKerning, out float kerningZ)
+    {
+        Vector2 output = Vector2.Zero;
+        output.Y = font.LineSpacing;
+
+        SpriteCharacterData characterData = font.GetCharacterData(c);
+        Vector3 kerning = characterData.Kerning;
+
+        if (firstChar)
+            kerning.X = Math.Max(kerning.X, 0f);
+        else
+            output.X += font.CharacterSpacing + lastKerning;
+
+        output.X += kerning.X + kerning.Y;
+
+        output.Y = Math.Max(output.Y, characterData.Padding.Height);
+
+        output.X += Math.Max(kerning.Z, 0f);
+
+        kerningZ = kerning.Z;
+
+        return output;
+    }
+
+    extension(Keys key)
+    {
+        public bool Pressed =>
+            Main.inputText.IsKeyDown(key);
+
+        public bool Held =>
+            Main.inputText.IsKeyDown(key) &&
+            Main.oldInputText.IsKeyDown(key);
+
+        public bool JustPressed =>
+            Main.inputText.IsKeyDown(key) &&
+            !Main.oldInputText.IsKeyDown(key);
+
+        public bool JustReleased =>
+            !Main.inputText.IsKeyDown(key) &&
+            Main.oldInputText.IsKeyDown(key);
+    }
+}
