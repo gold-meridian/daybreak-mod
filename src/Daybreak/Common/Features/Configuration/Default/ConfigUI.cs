8ae0d0908146e768e948f2d0b00cc4cd0dc6ec29
ZenTheMod <88523932+ZenTheMod@users.noreply.github.com>
2025-12-12 18:13:27 -0800

    (evil) move list fading to seperate type

--- src/Daybreak/Common/Features/Configuration/Default/ConfigUI.cs
+++ src/Daybreak/Common/Features/Configuration/Default/ConfigUI.cs
@@ -1,11 +1,13 @@
-﻿using Daybreak.Core;
+﻿using Daybreak.Common.Features.Configuration;
+using Daybreak.Common.Rendering;
+using Daybreak.Common.UI;
+using Daybreak.Core;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Daybreak.Common.Rendering;
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent.UI.Elements;
@@ -17,7 +19,6 @@ using Terraria.ModLoader.Default;
 using Terraria.ModLoader.UI;
 using Terraria.UI;
 using Terraria.UI.Gamepad;
-using Daybreak.Common.Features.Configuration;
 
 namespace Daybreak.Common.Features.Configuration.Default;
 
@@ -490,7 +491,7 @@ internal abstract class ConfigState : UIState, IHaveBackButtonCommand
 #endregion
 }
 
-public class CategoryTabList : UIList
+public class CategoryTabList : FadedList
 {
     public event Action<ConfigCategory>? OnCategorySelected;
 
@@ -595,35 +596,6 @@ public class CategoryTabList : UIList
             OnCategorySelected?.Invoke(Category);
             SoundEngine.PlaySound(SoundID.MenuOpen);
         }
-    }
-
-    protected override void DrawChildren(SpriteBatch spriteBatch)
-    {
-        AssetReferences.Assets.Shaders.UI.SlightListFade.Asset.Wait();
-
-        using var rtLease = ScreenspaceTargetPool.Shared.Rent(
-            Main.instance.GraphicsDevice,
-            RenderTargetDescriptor.DefaultPreserveContents
-        );
-
-        spriteBatch.End(out var ss);
-
-        using (rtLease.Scope(preserveContents: true, clearColor: Color.Transparent))
-        {
-            spriteBatch.Begin(ss);
-            base.DrawChildren(spriteBatch);
-            spriteBatch.End();
-        }
-
-        spriteBatch.Begin(ss with { SortMode = SpriteSortMode.Immediate });
-
-        var fadeShader = AssetReferences.Assets.Shaders.UI.SlightListFade.CreateFadeShader();
-        fadeShader.Parameters.uPanelDimensions = new Vector4(_dimensions.X, _dimensions.Y, _dimensions.Width, _dimensions.Height);
-        fadeShader.Parameters.uScreenSize = new Vector2(rtLease.Target.Width, rtLease.Target.Height);
-        fadeShader.Apply();
-
-        spriteBatch.Draw(rtLease.Target, _dimensions.Position(), _dimensions.ToRectangle(), Color.White);
-        spriteBatch.Restart(ss);
     }
 
     public class ModHeader : UIAutoScaleTextTextPanel<string>
---
+++ src/Daybreak/Common/UI/FadedList.cs
@@ -0,0 +1,40 @@
+﻿using Daybreak.Common.Rendering;
+using Daybreak.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+
+namespace Daybreak.Common.UI;
+
+public class FadedList : UIList
+{
+    protected override void DrawChildren(SpriteBatch spriteBatch)
+    {
+        AssetReferences.Assets.Shaders.UI.SlightListFade.Asset.Wait();
+
+        using var rtLease = ScreenspaceTargetPool.Shared.Rent(
+            Main.instance.GraphicsDevice,
+            RenderTargetDescriptor.DefaultPreserveContents
+        );
+
+        spriteBatch.End(out var ss);
+
+        using (rtLease.Scope(preserveContents: true, clearColor: Color.Transparent))
+        {
+            spriteBatch.Begin(ss);
+            base.DrawChildren(spriteBatch);
+            spriteBatch.End();
+        }
+
+        spriteBatch.Begin(ss with { SortMode = SpriteSortMode.Immediate });
+
+        var fadeShader = AssetReferences.Assets.Shaders.UI.SlightListFade.CreateFadeShader();
+        fadeShader.Parameters.uPanelDimensions = new Vector4(_dimensions.X, _dimensions.Y, _dimensions.Width, _dimensions.Height);
+        fadeShader.Parameters.uScreenSize = new Vector2(rtLease.Target.Width, rtLease.Target.Height);
+        fadeShader.Apply();
+
+        spriteBatch.Draw(rtLease.Target, _dimensions.Position(), _dimensions.ToRectangle(), Color.White);
+        spriteBatch.Restart(ss);
+    }
+}
