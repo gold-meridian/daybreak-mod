622678f4885463ac3a51248149772355f0987630
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-12-23 22:30:32 -0600

    feat: init primitive noise API

---
+++ src/Daybreak/Common/Mathematics/Noise/Noise.cs
@@ -0,0 +1,31 @@
+﻿namespace Daybreak.Common.Mathematics;
+
+/// <summary>
+///     Utilities for generating noise.
+/// </summary>
+public static class Noise
+{
+#region SimplexFbm
+#endregion
+
+#region FastValueFbm
+#endregion
+
+#region SimplexBillow
+#endregion
+
+#region SimplexTurbulence
+#endregion
+
+#region HybridMultifractal
+#endregion
+
+#region CellularFbm
+#endregion
+
+#region DomainWarpVector
+#endregion
+
+#region CurlSimplex
+#endregion
+}
