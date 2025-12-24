786a1ed477ac5dc671806b70e9b89379e6497461
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-12-23 22:39:55 -0600

    feat: genericized fbm function for any 2d noise

---
+++ src/Daybreak/Common/Mathematics/Noise/NoiseOperations.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+
+namespace Daybreak.Common.Mathematics;
+
+/// <summary>
+///     Generic operations on functions implementing
+///     <see cref="INoise2d{TSelf,TSettings}"/>.
+/// </summary>
+public static class NoiseOperations
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Fbm<TNoise, TSettings>(
+        Vector2 p,
+        int octaves = 4,
+        float lacunarity = 2f,
+        float gain = 0.5f,
+        float scale = 1f
+    )
+        where TNoise : INoise2d<TNoise, TSettings>
+        where TSettings : INoise2dSettings<TSettings>
+    {
+        return Fbm<TNoise, TSettings>(
+            p,
+            TSettings.DefaultSettings(),
+            octaves,
+            lacunarity,
+            gain,
+            scale
+        );
+    }
+
+    public static float Fbm<TNoise, TSettings>(
+        Vector2 p,
+        TSettings settings,
+        int octaves = 4,
+        float lacunarity = 2f,
+        float gain = 0.5f,
+        float scale = 1f
+    )
+        where TNoise : INoise2d<TNoise, TSettings>
+        where TSettings : INoise2dSettings<TSettings>
+    {
+        var amp = 1f;
+        var freq = 1f;
+        var sum = 0f;
+        var norm = 0f;
+
+        var seed = settings.Seed;
+        for (var i = 0; i < octaves; i++)
+        {
+            settings.Seed = seed + i * 53;
+
+            sum += TNoise.Sample(p * freq * scale, settings) * amp;
+            norm += amp;
+            amp *= gain;
+            freq *= lacunarity;
+        }
+
+        return norm > 0f ? sum / norm : 0.5f;
+    }
+}
