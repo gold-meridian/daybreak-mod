92dbcc9cec824fbc241d11751b6f5c1a15a2166c
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-12-23 22:56:15 -0600

    feat: genericized hybrid multifractal with default simplex overload

--- src/Daybreak/Common/Mathematics/Noise/NoiseOperations.cs
+++ src/Daybreak/Common/Mathematics/Noise/NoiseOperations.cs
@@ -153,4 +153,100 @@ public static class NoiseOperations
         return norm > 0f ? Math.Clamp(sum / norm, 0f, 1f) : 0.5f;
     }
 #endregion
+
+#region Hybrid Multifractal
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float HybridMultifractal(
+        Vector2 p,
+        int octaves = 5,
+        float lacunarity = 2f,
+        float gain = 0.5f,
+        float offset = 0.7f,
+        float scale = 1f
+    )
+    {
+        return HybridMultifractal<FastSimplexNoise>(
+            p,
+            octaves,
+            lacunarity,
+            gain,
+            scale
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float HybridMultifractal(
+        Vector2 p,
+        FastSimplexNoise settings,
+        int octaves = 5,
+        float lacunarity = 2f,
+        float gain = 0.5f,
+        float offset = 0.7f,
+        float scale = 1f
+    )
+    {
+        return HybridMultifractal<FastSimplexNoise>(
+            p,
+            settings,
+            octaves,
+            lacunarity,
+            gain,
+            scale
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float HybridMultifractal<TNoise>(
+        Vector2 p,
+        int octaves = 5,
+        float lacunarity = 2f,
+        float gain = 0.5f,
+        float offset = 0.7f,
+        float scale = 1f
+    ) where TNoise : unmanaged, INoise2d<TNoise>
+    {
+        return HybridMultifractal(
+            p,
+            TNoise.DefaultSettings(),
+            octaves,
+            lacunarity,
+            gain,
+            scale
+        );
+    }
+
+    public static float HybridMultifractal<TNoise>(
+        Vector2 p,
+        TNoise settings,
+        int octaves = 5,
+        float lacunarity = 2f,
+        float gain = 0.5f,
+        float offset = 0.7f,
+        float scale = 1f
+    ) where TNoise : unmanaged, INoise2d<TNoise>
+    {
+        var freq = 1f;
+        var weight = 1f;
+        var value = TNoise.Sample(p * freq * scale, settings) + offset;
+        var amp = 1f;
+
+        for (var i = 1; i < octaves; i++)
+        {
+            if (weight <= 0f)
+            {
+                break;
+            }
+
+            freq *= lacunarity;
+            amp *= gain;
+
+            var signal = TNoise.Sample(p * freq * scale, settings with { Seed = settings.Seed + i * 131 }) + offset;
+            signal *= amp;
+            value += weight * signal;
+            weight *= signal;
+        }
+
+        return Math.Clamp(value * 0.5f, 0f, 1f);
+    }
+#endregion
 }
