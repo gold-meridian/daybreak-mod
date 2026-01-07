e9b42c031984435536657860c86fe6b74bca103b
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-01-07 16:38:47 -0600

    feat: justified grids

--- src/Daybreak/Content/Compatibility/TerrariaOverhaulCompat.cs
+++ src/Daybreak/Content/Compatibility/TerrariaOverhaulCompat.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Daybreak.Common.CodeAnalysis;
 using Daybreak.Common.Features.Configuration;
 using Daybreak.Common.Features.Hooks;
+using Daybreak.Common.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using MonoMod.Cil;
+using ReLogic.Content;
+using Terraria;
 using Terraria.Audio;
+using Terraria.GameContent;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ID;
 using Terraria.Localization;
@@ -27,17 +34,140 @@ internal static class TerrariaOverhaulCompat
 {
     private sealed class RecalculableElement : UIElement
     {
-        public event Action OnRecalculate;
-        
+        public event Action? OnRecalculate;
+
         public override void Recalculate()
         {
             OnRecalculate?.Invoke();
             base.Recalculate();
         }
     }
-    
+
+    private sealed class FancierGrid : UIGrid
+    {
+        public override void RecalculateChildren()
+        {
+            var maxRowWidth = GetInnerDimensions().Width;
+
+            base.RecalculateChildren();
+
+            var index = 0;
+            var rowY = 0f;
+
+            var widths = (Span<int>)stackalloc int[_items.Count];
+            
+            do
+            {
+                var maxItemHeight = 0f;
+                var rowWidth = 0f;
+                var rowLength = 0;
+
+                for (var i = index; i < _items.Count; i++)
+                {
+                    var item = _items[i];
+
+                    var parentDimensions = item.Parent?.GetInnerDimensions() ?? UserInterface.ActiveInstance.GetDimensions();
+                    var dim = item.GetOuterDimensions();
+                    var minWidth = item.MinWidth.GetValue(parentDimensions.Width);
+                    
+                    if (rowLength > 0 && rowWidth + minWidth > maxRowWidth)
+                    {
+                        break;
+                    }
+
+                    rowWidth += minWidth;
+                    if (rowLength > 0)
+                    {
+                        rowWidth += ListPadding;
+                    }
+
+                    maxItemHeight = Math.Max(maxItemHeight, dim.Height);
+                    rowLength++;
+                }
+
+                if (rowLength == 0)
+                {
+                    break;
+                }
+
+                var totalPadding = ListPadding * (rowLength - 1);
+                var minContentWidth = rowWidth - totalPadding;
+                var extraSpace = Math.Max(0f, maxRowWidth - (minContentWidth + totalPadding));
+                var extraPerItem = extraSpace / rowLength;
+
+                var x = 0f;
+
+                for (var i = 0; i < rowLength; i++)
+                {
+                    var item = _items[index + i];
+                    
+                    var parentDimensions = item.Parent?.GetInnerDimensions() ?? UserInterface.ActiveInstance.GetDimensions();
+                    // var dim = item.GetOuterDimensions();
+                    var minWidth = item.MinWidth.GetValue(parentDimensions.Width);
+                    var newWidth = minWidth + extraPerItem;
+
+                    item.Left.Set(x, 0f);
+                    item.Top.Set(rowY, 0f);
+
+                    item.Width.Set(newWidth, 0f);
+
+                    x += newWidth + ListPadding;
+                }
+
+                index += rowLength;
+                rowY += maxItemHeight + ListPadding;
+            }
+            while (index < _items.Count);
+        }
+    }
+
+    [ExtendsFromMod("TerrariaOverhaul", "ThisModDoesNotExist_d426fe6781e47f3ca724d06792c60a12")]
+    private sealed class NewCategoryCardPanel(
+        string category,
+        Asset<Texture2D>? backgroundTexture = null,
+        Asset<Texture2D>? borderTexture = null
+    ) : CategoryCardPanel(category, backgroundTexture, borderTexture)
+    {
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            // base.DrawSelf(spriteBatch);
+
+            DrawStaticHighlightPanel(
+                spriteBatch,
+                IsMouseHovering ? BackgroundColor : BackgroundColor.MultiplyRGBA(new Color(180, 180, 180)),
+                this.Dimensions
+            );
+        }
+
+        private static void DrawStaticHighlightPanel(
+            SpriteBatch sb,
+            Color color,
+            Rectangle dims
+        )
+        {
+            const int split = 15;
+
+            TextureAssets.SettingsPanel.Wait();
+            var texture = TextureAssets.SettingsPanel.Value;
+
+            // Left/Right bars.
+            sb.Draw(texture, new Rectangle(dims.X, dims.Y + 2, 2, dims.Height - 4), new Rectangle(0, 2, 1, 1), color);
+            sb.Draw(texture, new Rectangle(dims.X + dims.Width - 2, dims.Y + 2, 2, dims.Height - 4), new Rectangle(0, 2, 1, 1), color);
+
+            // Up/Down bars.
+            sb.Draw(texture, new Rectangle(dims.X + 2, dims.Y, dims.Width - 4, 2), new Rectangle(2, 0, 1, 1), color);
+            sb.Draw(texture, new Rectangle(dims.X + 2, dims.Y + dims.Height - 2, dims.Width - 4, 2), new Rectangle(2, 0, 1, 1), color);
+
+            // Inner Panel.
+            sb.Draw(texture, new Rectangle(dims.X + 2, dims.Y + 2, dims.Width - 4, split - 2), new Rectangle(2, 2, 1, 1), color);
+            sb.Draw(texture, new Rectangle(dims.X + 2, dims.Y + split, dims.Width - 4, dims.Height - split - 2), new Rectangle(2, 16, 1, 1), color);
+        }
+    }
+
+    [ExtendsFromMod("TerrariaOverhaul")]
     private sealed class SuperSpecialOverhaulCardPageProvider : IDefaultModPageProvider
     {
+        [NoJIT]
         void IDefaultModPageProvider.AddCategoriesToContainer(UIList container, ConfigCategory[] categories)
         {
             var gridContainer = new RecalculableElement();
@@ -47,37 +177,50 @@ internal static class TerrariaOverhaulCompat
             }
             container.Add(gridContainer);
 
-            var grid = new FancyUIGrid();
+            var grid = new FancierGrid();
             {
-                grid.Width.Set(-20f, 1f);
+                grid.ManualSortMethod = _ => { };
+                grid.Width.Set(0f, 1f);
                 grid.Height.Set(0f, 1f);
-                grid.ListPadding = 15f;
-                grid.PaddingRight = 15f;
-                
+                grid.ListPadding = 8f;
+                grid.SetPadding(0f);
+
                 gridContainer.OnRecalculate += () =>
                 {
                     gridContainer.Height.Set(grid.GetTotalHeight(), 0f);
                 };
             }
             gridContainer.Append(grid);
-            
+
             var i = 0;
             foreach (var category in categories.OrderBy(x => x.Handle.Name))
             {
-                var cardPanel = new CategoryCardPanel(category.Handle.Name, CommonAssets.GetBackgroundTexture(i++))
+                var cardPanel = new NewCategoryCardPanel(category.Handle.Name, CommonAssets.GetBackgroundTexture(i++))
                 {
                     UserObject = category.Handle.Name,
                 };
                 {
+                    cardPanel.MinWidth = cardPanel.Width;
                     cardPanel.OnLeftClick += (_, _) =>
                     {
                         SoundEngine.PlaySound(SoundID.MenuOpen);
                         // SetCategoryScreen(category);
                     };
                 }
+
+                if (!TerrariaOverhaul.Core.Configuration.ConfigSystem.CategoriesByName.TryGetValue(category.Handle.Name, out var categoryData))
+                {
+                    continue;
+                }
+
+                if (categoryData.EntriesByName.Values.All(x => x.IsHidden))
+                {
+                    continue;
+                }
+                
                 grid.Add(cardPanel);
             }
-            
+
             container.Recalculate();
         }
     }
