494540b632787801c66b0e8c4da0b775a7355819
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-05-14 00:46:45 -0500

    feat: gut lsp and throw error on load

---
+++ src/LiquidSlopesPatch/ModImpl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LiquidSlopesPatch;
+
+partial class ModImpl
+{
+    public override void Load()
+    {
+        base.Load();
+
+        throw new Exception("Liquid Slopes Patch has been merged into tModLoader and is no longer required. Please disable it.");
+    }
+}
