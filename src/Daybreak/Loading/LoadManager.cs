22eda2152c2c43baa9bc3ffd04abffc901fe728a
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-05-14 04:11:46 -0500

    feat: outline automatic module registration system

---
+++ src/Daybreak/Loading/LoadManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Daybreak.Loading;
+
+/// <summary>
+///     Responsible for carrying out various loading tasks for Daybreak and its
+///     module management.
+/// </summary>
+internal static class LoadManager
+{
+    /// <summary>
+    ///     Registers a module.
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <param name="parentMod"></param>
+    public static void RegisterModule(Assembly assembly, string? parentMod)
+    {
+        
+    }
+
+    private static void RegisterModule(string loadingMod, Assembly assembly, string? parentMod)
+    {
+        
+    }
+}
--- src/modules/shared/DaybreakModuleAttribute.cs
+++ src/modules/shared/DaybreakModuleAttribute.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
+using Terraria.ModLoader.Core;
 
 namespace Daybreak;
 
@@ -24,7 +27,67 @@ namespace Daybreak;
 [AttributeUsage(AttributeTargets.Assembly)]
 internal sealed class DaybreakModuleAttribute(bool loadCycle = true, string? parentMod = null) : Attribute
 {
-    public bool ParticipatesInLoadCycle => loadCycle;
+    private static readonly DaybreakModuleAttribute default_attribute = new(loadCycle: false, parentMod: null);
 
-    public string? ParentMod => parentMod;
+    private bool ParticipatesInLoadCycle => loadCycle;
+
+    private string? ParentMod => parentMod;
+
+    private const string daybreak_mod = "Daybreak";
+    private const string daybreak_load_manager_type = "Daybreak.Loading.LoadManager";
+    private const string load_manager_register_module_method = "RegisterModule";
+
+#pragma warning disable CA2255
+    [ModuleInitializer]
+    public static void VerifyLoadingEnvironment()
+    {
+        var asm = typeof(DaybreakModuleAttribute).Assembly;
+        var settings = asm.GetCustomAttribute<DaybreakModuleAttribute>()
+                    ?? default_attribute;
+
+        // Nothing to do.  If the module doesn't participate in the load cycle,
+        // it's not actually a Daybreak-style module and does not need special
+        // handling (nor does Daybreak need to know about its presence).
+        if (!settings.ParticipatesInLoadCycle)
+        {
+            return;
+        }
+
+        // ModuleInitializers run early enough in the load process that
+        // ModLoader.HasMod and related APIs simply won't work.  We can get
+        // around this by manually fiddling with the assembly load contexts.
+        if (!AssemblyManager.loadedModContexts.TryGetValue(daybreak_mod, out var daybreakAlc))
+        {
+            // This is primarily a safeguard against improper dependency
+            // management, typically a result of including modules in
+            // dllReferences when it's inappropriate.  Any modules that mark
+            // loadCycle as true (default) should never be dllReferences'd by
+            // anything other than the mod whose responsibility it is to
+            // distribute the module to other mods.
+            // That means this error will basically only throw when a dependent
+            // mod includes a module as a dllReference, which it should never
+            // do.
+            // If a mod is strongly referencing Daybreak (or whatever mod is
+            // providing the module they need), they only need to modReference
+            // or weakReference the providing mod, as dependencies will be
+            // transiently provided.
+            // If a mod fails to load with weak references alone, you just need
+            // to consult the tModLoader Expert Cross Mod Guide. It will teach
+            // you how to only load assemblies when they're actually needed.
+            throw new InvalidOperationException(
+                "An error occurred loading a DAYBREAK module. THIS IS *NOT* DAYBREAK'S FAULT, tModLoader should indicate the mod that made the mistake."
+              + "\n\nAttempted to load a Daybreak-style module without Daybreak loaded; this typically means the offending mod is not properly referencing one or more dependencies."
+              + "\n\nIf you are a player, report this to the currently-loading mod, which tModLoader should indicate."
+              + "\n\nIf you are the developer, you're referencing a module in dllReferences when you shouldn't be. If you don't know how to fix this, contact a DAYBREAK developer through the mod homepage."
+            );
+        }
+
+        // Transfer handling to the loaded Daybreak copy now, so we can record
+        // the module's presence.
+        daybreakAlc.assembly
+                   .GetType(daybreak_load_manager_type)!
+                   .GetMethod(load_manager_register_module_method, BindingFlags.Public | BindingFlags.Static, [typeof(Assembly), typeof(string)])!
+                   .Invoke(null, [asm, settings.ParentMod]);
+    }
+#pragma warning restore CA2255
 }
