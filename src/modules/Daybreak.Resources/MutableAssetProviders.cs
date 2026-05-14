526b7c7de6d6c8a11f70944ab74f617360248ece
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-05-14 03:16:06 -0500

    feat: port assetreplacer with a partially revised api

---
+++ src/modules/Daybreak.Resources/MutableAssetProviders.cs
@@ -0,0 +1,27 @@
+﻿using ReLogic.Content;
+
+namespace Daybreak.Resources;
+
+/// <summary>
+///     Provides an API to get and set the value of an asset of type
+///     <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">The asset type.</typeparam>
+public interface IMutableAssetProvider<T>
+    where T : class
+{
+    /// <summary>
+    ///     The mutable asset.
+    /// </summary>
+    T Asset { get; set; }
+}
+
+internal readonly struct ReLogicMutableAssetProvider<T>(Asset<T> source) : IMutableAssetProvider<T>
+    where T : class
+{
+    public T Asset
+    {
+        get => source.Value;
+        set => source.ownValue = value;
+    }
+}
