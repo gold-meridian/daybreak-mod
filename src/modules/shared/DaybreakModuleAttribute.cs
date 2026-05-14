639b9e18d0e58ea37aa1e95dd2ca035e56b66226
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-05-14 03:36:53 -0500

    feat: include option for whether the module actually participates in the load cycle

--- src/modules/shared/DaybreakModuleAttribute.cs
+++ src/modules/shared/DaybreakModuleAttribute.cs
@@ -10,6 +10,10 @@ namespace Daybreak;
 ///     This enables an assembly to be included in the loading cycle of a parent
 ///     mod as if it were part of the same assembly.
 /// </summary>
+/// <param name="loadCycle">
+///     Whether the assembly actually participates in the including mod's load
+///     cycle.
+/// </param>
 /// <param name="parentMod">
 ///     If specified, indicates this module is only valid to be loaded by the
 ///     named mod.  Use this for modules that may be dependencies of other mods
@@ -18,7 +22,9 @@ namespace Daybreak;
 /// </param>
 [UsedImplicitly]
 [AttributeUsage(AttributeTargets.Assembly)]
-internal sealed class DaybreakModuleAttribute(string? parentMod = null) : Attribute
+internal sealed class DaybreakModuleAttribute(bool loadCycle = true, string? parentMod = null) : Attribute
 {
+    public bool ParticipatesInLoadCycle => loadCycle;
+
     public string? ParentMod => parentMod;
 }
