338896c0a74d9dffed39e155deb1f32cd0d7d1f5
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-05-14 03:34:36 -0500

    feat: Asset<T>::ImmediateValue

---
+++ src/modules/Daybreak.Resources/AssetExtensions.cs
@@ -0,0 +1,30 @@
+﻿using ReLogic.Content;
+
+namespace Daybreak.Resources;
+
+/// <summary>
+///     Extensions to <see cref="Asset{T}"/>.
+/// </summary>
+public static class AssetExtensions
+{
+    extension<T>(Asset<T> asset)
+        where T : class
+    {
+        /// <summary>
+        ///     Returns the value of the asset immediately, blocking if it isn't
+        ///     yet loaded.
+        /// </summary>
+        public T ImmediateValue
+        {
+            get
+            {
+                if (asset.State == AssetState.Loading)
+                {
+                    asset.Wait();
+                }
+
+                return asset.Value;
+            }
+        }
+    }
+}
