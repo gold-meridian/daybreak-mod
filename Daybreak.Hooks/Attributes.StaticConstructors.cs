a2c7099e79b14e113ed5a3cda2d76e2a8cdcd403
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-06-04 11:58:20 -0500

    feat: move static ctor attributes to their own file

---
+++ Daybreak.Hooks/Attributes.StaticConstructors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Daybreak.Hooks;
+
+/// <summary>
+///     Any types decorated with this attribute will cause a type to run its
+///     static constructor if it contains static fields or properties of the
+///     type.  Use <see cref="DontForceStaticConstructorAttribute"/> for members
+///     that should not respect this behavior.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public sealed class RunsStaticConstructorsAttribute : Attribute;
+
+/// <summary>
+///     Overrides the behavior of <see cref="RunsStaticConstructorsAttribute"/>
+///     to not apply for the given member.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field)]
+public sealed class DontForceStaticConstructorAttribute : Attribute;
