b84f0c0084fc2c879f59ee0034a6aa32d82b4af4
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-06-04 12:19:33 -0500

    feat: start porting over hook loader stuff

---
+++ src/Daybreak.EarlyLoader/EarlyLoadHooks.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using MonoMod.Cil;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
+using Terraria.ModLoader.Default;
+
+namespace Daybreak.EarlyLoader;
+
+/// <summary>
+///     Exposes convenient early loading hooks for mods.
+/// </summary>
+public static class EarlyLoadHooks
+{
+    private static Mod? currentlyLoadingMod;
+    private static Mod? currentlyUnloadingMod;
+
+    private static readonly Dictionary<nint, bool> type_runs_cctor = [];
+    private static readonly Dictionary<Assembly, Type[]> loadable_types = [];
+    private static readonly string assembly_name = typeof(HookLoader).Assembly.GetName().Name ?? "Daybreak";
+
+    public static Mod GetModOrThrow()
+    {
+        return currentlyLoadingMod ?? throw new InvalidOperationException("Cannot continue with operation as no mod is currently applicable to load content through Mod::AddContent");
+    }
+
+#pragma warning disable CA2255
+    [ModuleInitializer]
+    internal static void HookIntoContentLoadingRoutine()
+    {
+        // Hijack the content loading process to allow us to handle every added
+        // loadable.  For every loadable, resolve instanced hooks and apply
+        // them.
+        MonoModHooks.Add(
+            typeof(Mod).GetMethods().Single(x => x.Name.Equals(nameof(Mod.AddContent)) && !x.IsGenericMethod),
+            AddContent_ResolveInstancedHooks_CallOnLoads
+        );
+
+        MonoModHooks.Add(
+            typeof(Mod).GetMethod(nameof(Mod.Autoload), BindingFlags.NonPublic | BindingFlags.Instance)!,
+            Autoload_ResolveStaticHooks_CallOnLoads
+        );
+
+        /*
+        MonoModHooks.Add(
+            typeof(MenuLoader).GetMethod(nameof(MenuLoader.Unload), BindingFlags.NonPublic | BindingFlags.Static)!,
+            Unload_CallOnUnloads
+        );
+        */
+
+        MonoModHooks.Add(
+            typeof(MonoModHooks).GetMethod(nameof(MonoModHooks.RemoveAll), BindingFlags.NonPublic | BindingFlags.Static)!,
+            RemoveAll_SkipUs
+        );
+
+        MonoModHooks.Modify(
+            typeof(Mod).GetMethod(nameof(Mod.UnloadContent), BindingFlags.NonPublic | BindingFlags.Instance)!,
+            UnloadContent_CallOnUnloads
+        );
+
+        MonoModHooks.Add(
+            typeof(Mod).GetMethod(nameof(Mod.UnloadContent), BindingFlags.NonPublic | BindingFlags.Instance)!,
+            UnloadContent_WrapToMarkUnloadingMod
+        );
+
+        // AutoloadConfig and EnsureResizeArraysAttributeStaticCtorsRun are the
+        // first and last methods called in the routine where Mod::loading is
+        // set to true.  We use these to know what mod is currently being
+        // loaded.
+        MonoModHooks.Add(
+            typeof(Mod).GetMethod(nameof(Mod.AutoloadConfig), BindingFlags.NonPublic | BindingFlags.Instance),
+            (Action<Mod> orig, Mod self) =>
+            {
+                Debug.Assert(currentlyLoadingMod is null);
+
+                currentlyLoadingMod = self;
+
+                OnEarlyModLoad?.Invoke(self);
+
+                orig(self);
+            }
+        );
+
+        MonoModHooks.Add(
+            typeof(SystemLoader).GetMethod(nameof(SystemLoader.EnsureResizeArraysAttributeStaticCtorsRun), BindingFlags.NonPublic | BindingFlags.Static),
+            (Action<Mod> orig, Mod mod) =>
+            {
+                Debug.Assert(currentlyLoadingMod == mod);
+
+                orig(mod);
+                currentlyLoadingMod = null;
+            }
+        );
+    }
+#pragma warning restore CA2255
+
+    private static bool ModIsEligibleForSpecialLoading(Mod mod)
+    {
+        // TODO: Could also walk the weakReferences/modReferences trees?
+        return mod.Name == ModuleLoader.OwningMod || mod.Code.GetReferencedAssemblies().Any(x => x.Name == ModuleLoader.OwningMod);
+    }
+
+    private static bool AddContent_ResolveInstancedHooks_CallOnLoads(Func<Mod, ILoadable, bool> orig, Mod self, ILoadable instance)
+    {
+        // Only attempt to resolve and apply hooks if the instance actually
+        // loaded...
+        if (!orig(self, instance))
+        {
+            return false;
+        }
+
+        ContractEnforcer.ValidateLoadable(instance);
+
+        ResolveInstancedHooks(instance);
+        CallOnLoads(instance, self);
+        return true;
+    }
+
+    private static void Autoload_ResolveStaticHooks_CallOnLoads(Action<Mod> orig, Mod self)
+    {
+        orig(self);
+
+        if (self.Code is null || !ModIsEligibleForDaybreakLoading(self))
+        {
+            return;
+        }
+
+        var loadableTypes = loadable_types[self.Code] =
+            AssemblyManager.GetLoadableTypes(self.Code)
+                           .Where(x => !x.IsEnum && !x.IsSubclassOf(typeof(MulticastDelegate)) && AutoloadAttribute.GetValue(x).NeedsAutoloading)
+                           .OrderBy(x => x.FullName, StringComparer.InvariantCulture)
+                           .ToArray();
+
+        LoaderUtils.ForEachAndAggregateExceptions(loadableTypes, ResolveStaticHooks);
+        LoaderUtils.ForEachAndAggregateExceptions(loadableTypes, CallOnLoads);
+        LoaderUtils.ForEachAndAggregateExceptions(loadableTypes.Where(self.PreJITFilter.ShouldJIT), RunStaticConstructors);
+    }
+
+    /*
+    private static void Unload_CallOnUnloads(Action orig)
+    {
+        foreach (var mod in ModLoader.Mods)
+        {
+            var loadableTypes = AssemblyManager.GetLoadableTypes(mod.Code)
+                                               .OrderBy(x => x.FullName, StringComparer.InvariantCulture)
+                                               .ToArray();
+            LoaderUtils.ForEachAndAggregateExceptions(Enumerable.Reverse(loadableTypes), CallOnUnloads);
+
+            foreach (var loadable in mod.GetContent().Reverse())
+            {
+                CallOnUnloads(loadable);
+            }
+        }
+
+        orig();
+    }
+    */
+
+    private static void RemoveAll_SkipUs(Action<Mod> orig, Mod mod)
+    {
+        if (mod.Name == ModuleLoader.OwningMod)
+        {
+            return;
+        }
+
+        // Force our edits to unload when it gets to ModLoaderMod instead (late
+        // stage).
+        // Use TryGetMod because GetInstance may return null in early unload stages.
+        if (mod is ModLoaderMod && ModLoader.TryGetMod("Daybreak", out var modImpl))
+        {
+            orig(modImpl);
+            return;
+        }
+
+        orig(mod);
+    }
+
+    private static void UnloadContent_CallOnUnloads(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(MoveType.After, x => x.MatchCallvirt<Mod>(nameof(Mod.Unload)));
+
+        c.EmitLdarg0();
+        c.EmitDelegate(
+            static (Mod mod) =>
+            {
+                if (!loadable_types.TryGetValue(mod.Code, out var loadableTypes))
+                {
+                    return;
+                }
+
+                loadableTypes = loadableTypes.AsEnumerable().Reverse().ToArray();
+
+                LoaderUtils.ForEachAndAggregateExceptions(Enumerable.Reverse(loadableTypes), CallOnUnloads);
+            }
+        );
+
+        c.GotoNext(MoveType.Before, x => x.MatchCallvirt<ILoadable>(nameof(ILoadable.Unload)));
+
+        c.EmitDup();
+        c.EmitDelegate(
+            static (ILoadable loadable) =>
+            {
+                CallOnUnloads(loadable);
+            }
+        );
+    }
+
+    private static void UnloadContent_WrapToMarkUnloadingMod(Action<Mod> orig, Mod mod)
+    {
+        currentlyUnloadingMod = mod;
+        OnEarlyModUnload?.Invoke(mod);
+        orig(mod);
+        currentlyUnloadingMod = null;
+    }
+
+    private static void CallOnUnloads(ILoadable instance)
+    {
+        Debug.Assert(currentlyUnloadingMod is not null);
+
+        var methods = instance.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        foreach (var method in Enumerable.Reverse(methods))
+        {
+            var attribute = method.GetCustomAttribute<OnUnloadAttribute>(inherit: false);
+            if (attribute is null)
+            {
+                continue;
+            }
+
+            if (!ModOrganizer.LoadSide(attribute.Side))
+            {
+                continue;
+            }
+
+            if (method.IsGenericMethod)
+            {
+                throw new InvalidOperationException($"The method {method} cannot be generic.");
+            }
+
+            if (method.ReturnType != typeof(void))
+            {
+                throw new InvalidOperationException($"The method {method} must return void.");
+            }
+
+            HookSubscriber.BuildWrapper<OnUnloadHook.Definition>(method, instance)
+                          .Invoke(currentlyUnloadingMod);
+        }
+    }
+
+    private static void CallOnUnloads(Type type)
+    {
+        Debug.Assert(currentlyUnloadingMod is not null);
+
+        var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+        foreach (var method in Enumerable.Reverse(methods))
+        {
+            var attribute = method.GetCustomAttribute<OnUnloadAttribute>(inherit: false);
+            if (attribute is null)
+            {
+                continue;
+            }
+
+            if (!ModOrganizer.LoadSide(attribute.Side))
+            {
+                continue;
+            }
+
+            if (method.IsGenericMethod)
+            {
+                throw new InvalidOperationException($"The method {method} cannot be generic.");
+            }
+
+            if (method.ReturnType != typeof(void))
+            {
+                throw new InvalidOperationException($"The method {method} must return void.");
+            }
+
+            HookSubscriber.BuildWrapper<OnUnloadHook.Definition>(method, null)
+                          .Invoke(currentlyUnloadingMod);
+        }
+    }
+
+    private static void ResolveInstancedHooks(ILoadable instance)
+    {
+        SubscribeToHooks(
+            instance.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance),
+            instance
+        );
+    }
+
+    private static void ResolveStaticHooks(Type type)
+    {
+        SubscribeToHooks(
+            type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static),
+            null
+        );
+    }
+
+    private static void CallOnLoads(ILoadable instance, Mod mod)
+    {
+        // Normally we would use currentlyLoadingMod, but tPackBuilder breaks
+        // with this approach because it independently fucks up load orders.  We
+        // can just use the Mod instance passed to AddContent, thankfully.
+        // Debug.Assert(currentlyLoadingMod is not null);
+
+        var methods = instance.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        foreach (var method in methods)
+        {
+            var attribute = method.GetCustomAttribute<OnLoadAttribute>(inherit: false);
+            if (attribute is null)
+            {
+                continue;
+            }
+
+            if (!ModOrganizer.LoadSide(attribute.Side))
+            {
+                continue;
+            }
+
+            if (method.IsGenericMethod)
+            {
+                throw new InvalidOperationException($"The method {method} cannot be generic.");
+            }
+
+            if (method.ReturnType != typeof(void))
+            {
+                throw new InvalidOperationException($"The method {method} must return void.");
+            }
+
+            HookSubscriber.BuildWrapper<OnLoadHook.Definition>(method, instance)
+                          .Invoke( /*currentlyLoadingMod*/ mod);
+        }
+    }
+
+    private static void CallOnLoads(Type type)
+    {
+        Debug.Assert(currentlyLoadingMod is not null);
+
+        var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+        foreach (var method in methods)
+        {
+            var attribute = method.GetCustomAttribute<OnLoadAttribute>(inherit: false);
+            if (attribute is null)
+            {
+                continue;
+            }
+
+            if (!ModOrganizer.LoadSide(attribute.Side))
+            {
+                continue;
+            }
+
+            if (method.IsGenericMethod)
+            {
+                throw new InvalidOperationException($"The method {method} cannot be generic.");
+            }
+
+            if (method.ReturnType != typeof(void))
+            {
+                throw new InvalidOperationException($"The method {method} must return void.");
+            }
+
+            HookSubscriber.BuildWrapper<OnLoadHook.Definition>(method, null)
+                          .Invoke(currentlyLoadingMod);
+        }
+    }
+
+    private static void SubscribeToHooks(MethodInfo[] methods, object? instance)
+    {
+        foreach (var method in methods)
+        {
+            var attributes = method.GetCustomAttributes<BaseHookAttribute>(inherit: false);
+
+            foreach (var attribute in attributes)
+            {
+                if (attribute is IHasSide hasSide && !ModOrganizer.LoadSide(hasSide.Side))
+                {
+                    continue;
+                }
+
+                attribute.Apply(method, instance);
+            }
+        }
+    }
+
+    private static void RunStaticConstructors(Type type)
+    {
+        var fields = type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+        foreach (var field in fields)
+        {
+            var handle = field.FieldType.TypeHandle.Value;
+            if (!type_runs_cctor.TryGetValue(handle, out var runsStaticCtor))
+            {
+                runsStaticCtor = type_runs_cctor[handle] = field.FieldType.GetCustomAttribute<RunsStaticConstructorsAttribute>(inherit: false) is not null;
+            }
+
+            if (!runsStaticCtor)
+            {
+                continue;
+            }
+
+            if (field.GetCustomAttribute<DontForceStaticConstructorAttribute>(inherit: false) is not null)
+            {
+                continue;
+            }
+
+            RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+            return;
+        }
+    }
+}
--- src/DaybreakModuleAttribute.cs
+++ src/DaybreakModuleAttribute.cs
@@ -62,12 +62,13 @@ internal sealed class DaybreakModuleAttribute : Attribute
 /// <summary>
 ///     Responsible for handling the loading of the DAYBREAK Module.
 /// </summary>
-file static class ModuleLoader
+internal static class ModuleLoader
 {
-    private static ILog Logger => Logging.tML;
+    public static ILog Logger { get; } = LogManager.GetLogger(typeof(ModuleLoader));
+
+    public static string? OwningMod { get; private set; }
 
     private static readonly Assembly module_assembly = typeof(ModuleLoader).Assembly;
-    private static string? owningMod;
 
     public static void LoadModule(DaybreakModuleAttribute moduleSettings)
     {
@@ -84,14 +85,14 @@ file static class ModuleLoader
             throw new InvalidOperationException("Failed to load module; could not resolve owning AssemblyLoadContext!");
         }
 
-        owningMod = alc.Name;
-        Logger.Debug($"Module is being loaded by: {owningMod}");
-        if (expectedMod is not null && !string.Equals(owningMod, expectedMod, StringComparison.InvariantCultureIgnoreCase))
+        OwningMod = alc.Name;
+        Logger.Debug($"Module is being loaded by: {OwningMod}");
+        if (expectedMod is not null && !string.Equals(OwningMod, expectedMod, StringComparison.InvariantCultureIgnoreCase))
         {
             throw new InvalidOperationException(
-                $"The mod '{owningMod}' failed to load due to misconfigured DAYBREAK-style modules."
-              + $"\'{owningMod}' attempted to load the module '{module_assembly.GetName().FullName}', but the module may only be loaded by '{expectedMod}'!"
-              + $"\n\nIf you are a player, simply report this to the developers of '{owningMod}'."
+                $"The mod '{OwningMod}' failed to load due to misconfigured DAYBREAK-style modules."
+              + $"\'{OwningMod}' attempted to load the module '{module_assembly.GetName().FullName}', but the module may only be loaded by '{expectedMod}'!"
+              + $"\n\nIf you are a player, simply report this to the developers of '{OwningMod}'."
               + $"\n\nIf you're a developer, you have attempted to load an assembly owned by another mod.  Do not reference this module with dllReferences."
               + $"\nIf you're a developer and need help fixing this, talk to the DAYBREAK developers in the NIGHTSHADE Discord: discord.gg/yog"
             );
@@ -112,38 +113,38 @@ file static class ModuleLoader
     {
         Logger.Debug("Injecting into the mod load cycle...");
 
-        if (owningMod is null)
+        if (OwningMod is null)
         {
-            throw new InvalidOperationException($"Module had no resolved loading mod; {nameof(owningMod)} was null!");
+            throw new InvalidOperationException($"Module had no resolved loading mod; {nameof(OwningMod)} was null!");
         }
-        
+
         MonoModHooks.Add(
             typeof(AssemblyManager).GetMethod(nameof(AssemblyManager.GetLoadableTypes), BindingFlags.NonPublic | BindingFlags.Static, [typeof(AssemblyManager.ModLoadContext), typeof(MetadataLoadContext)])!,
             (Func<AssemblyManager.ModLoadContext, MetadataLoadContext, Dictionary<Assembly, Type[]>> orig, AssemblyManager.ModLoadContext mod, MetadataLoadContext mlc) =>
             {
                 var typeMap = orig(mod, mlc);
-                if (owningMod != mod.Name)
+                if (OwningMod != mod.Name)
                 {
                     return typeMap;
                 }
 
                 if (!typeMap.TryGetValue(module_assembly, out var types))
                 {
-                    Logger.Warn($"Failed to find assembly types in GetLoadableTypes type map for mod: {owningMod}");
+                    Logger.Warn($"Failed to find assembly types in GetLoadableTypes type map for mod: {OwningMod}");
                     return typeMap;
                 }
-                
+
                 // Clear it out to avoid duplicated logic but also to avoid
                 // null references, since I think a mod could try to request
                 // loadable types of our assembly by checking
                 // ModLoadContext::assemblies.
                 typeMap[module_assembly] = [];
-                
+
                 // Actually add our types to the main mod.  This is a little
                 // gross due to ordering, but tModLoader makes no guarantees
                 // about it, and we can call it an implementation detail.
                 typeMap[mod.assembly] = typeMap[mod.assembly].Concat(types).ToArray();
-                
+
                 return typeMap;
             }
         );
