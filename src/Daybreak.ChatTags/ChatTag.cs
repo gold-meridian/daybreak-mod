ee1c8177d11d32c9757e6114ba7fab44b8cfbac4
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-07-12 21:22:45 -0500

    feat: chat tags module draft

---
+++ src/Daybreak.ChatTags/ChatTag.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+using Terraria.UI.Chat;
+
+namespace Daybreak.ChatTags;
+
+/// <summary>
+///     An autoloaded chat tag.  Chat tags provide <see cref="TextSnippet"/>
+///     parsed from a tag: <c>[name/opts:text]</c> (or, optionally,
+///     <c>[name:text]</c>).
+/// </summary>
+public abstract class ChatTag : ModType, ITagHandler
+{
+    /// <summary>
+    ///     The primary name of this chat tag.
+    /// </summary>
+    public abstract string TagName { get; }
+
+    /// <summary>
+    ///     Additional names for the tag.
+    /// </summary>
+    public virtual string[] AliasNames { get; } = [];
+
+    /// <inheritdoc />
+    protected sealed override void Register()
+    {
+        ChatManager.Register(this, TagName, AliasNames);
+    }
+
+    /// <inheritdoc />
+    public sealed override void SetupContent()
+    {
+        base.SetupContent();
+
+        SetStaticDefaults();
+    }
+
+    /// <summary>
+    ///     Parses the text into the accompanying <see cref="TextSnippet"/>.
+    /// </summary>
+    /// <param name="text">The text to parse.  The body of the tag.</param>
+    /// <param name="baseColor">The base color to render with.</param>
+    /// <param name="options">
+    ///     Any options that are included after the tag name.
+    /// </param>
+    /// <returns>
+    ///     The parsed text snippet.
+    /// </returns>
+    public abstract TextSnippet Parse(
+        string text,
+        Color baseColor = new(),
+        string? options = null
+    );
+}
---
+++ src/Daybreak.ChatTags/ChatTags.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Terraria.GameContent.UI.Chat;
+using Terraria.UI.Chat;
+
+namespace Daybreak.ChatTags;
+
+/// <summary>
+///     Utilities and other APIs relating to chat tags.
+/// </summary>
+public static class ChatTags
+{
+    /// <summary>
+    ///     The handler for the color tag.
+    /// </summary>
+    public static ColorTagHandler Color => (ColorTagHandler)ChatManager.GetHandler("color");
+    
+    /// <summary>
+    ///     The handler for the item tag.
+    /// </summary>
+    public static ItemTagHandler Item => (ItemTagHandler)ChatManager.GetHandler("item");
+    
+    /// <summary>
+    ///     The handler for the name tag.
+    /// </summary>
+    public static NameTagHandler Name => (NameTagHandler)ChatManager.GetHandler("name");
+    
+    /// <summary>
+    ///     The handler for the achievement tag.
+    /// </summary>
+    public static AchievementTagHandler Achievement => (AchievementTagHandler)ChatManager.GetHandler("achievement");
+    
+    // FIXME: A bug in vanilla means we can't use the `glyph` tag here.  We have
+    //        to use the guaranteed `g` alias.  `glyph` *should* correspond to
+    //        the plugged-in controller's glyphs, but it gets overwritten.
+    /// <summary>
+    ///     The handler for the controller-dependent glyph tag.  This tag will
+    ///     show the glyph corresponding to the plugged-in controller.
+    /// </summary>
+    public static GlyphTagHandler Glyph => (GlyphTagHandler)ChatManager.GetHandler("g");
+    
+    /// <summary>
+    ///     The handler for the XBOX controller glyphs.
+    /// </summary>
+    public static GlyphTagHandler.GlyphXboxTagHandler XboxGlyph => (GlyphTagHandler.GlyphXboxTagHandler)ChatManager.GetHandler("gx");
+    
+    /// <summary>
+    ///     The handler for the PlayStation controller glyphs.
+    /// </summary>
+    public static GlyphTagHandler.GlyphPSTagHandler PsGlyph => (GlyphTagHandler.GlyphPSTagHandler)ChatManager.GetHandler("gp");
+    
+    /// <summary>
+    ///     The handler for the Nintendo Switch controller glyphs.
+    /// </summary>
+    public static GlyphTagHandler.GlyphSwitchTagHandler SwitchGlyph => (GlyphTagHandler.GlyphSwitchTagHandler)ChatManager.GetHandler("gn");
+    
+    extension(ChatManager)
+    {
+        // This overload exists to avoid CRTP in our ChatTag API and to avoid
+        // allocations when registering with aliases.
+        /// <summary>
+        ///     Registers the <paramref name="tag"/> instance under the primary
+        ///     <paramref name="name"/> and any possible
+        ///     <paramref name="aliases"/>.
+        /// </summary>
+        public static void Register(ITagHandler tag, string name, IEnumerable<string> aliases)
+        {
+            // We use ToLowerInvariant instead of ToLower... just in case.
+            ChatManager._handlers[name.ToLowerInvariant()] = tag;
+
+            foreach (var alias in aliases)
+            {
+                ChatManager._handlers[alias.ToLowerInvariant()] = tag;
+            }
+        }
+    }
+}
